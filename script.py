40256a19b7690f1ba4b888db78838fc7aa56ec21
sarah-lcfs <s.lacoffrette@cat-amania.com>
2025-03-24 10:42:32 +0100

    add :: adapt origin script

--- script.py
+++ script.py
@@ -1,87 +1,130 @@
+# V1.1 initial version
+
 import json
 import os
 import pymsteams
 from dotenv import load_dotenv
+import sys
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Chargement des variables d'environnement
+# Loading environment variables
 load_dotenv()
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Chemins des fichiers JSON
+# Env urls
+ENVIRONMENTS = {
+    "prod": "https://api.porsche-experience-center.fr",
+    "preprod": "https://api-porsche-fr-preprod.cat-amania.com",
+    "dev": "https://api-porsche-dev-oci.cat-amania.com"
+}
+
+# Check arguments
+#if len(sys.argv) != 2 or sys.argv[1] not in ENVIRONMENTS:
+#    print("Usage: python check_ms.py <environment>")
+ #   print("Possible values for <environment>: prod, preprod, dev")
+ #   sys.exit(1)
+
+#BASE_ADDRESS = ENVIRONMENTS[sys.argv[1]]
+
+# Actuator health endpoint path template
+HEALTH_ENDPOINT_TEMPLATE = "/{}/actuator/health"
+
+# JSON file paths
 MICROSERVICES_FILE = os.path.join(BASE_DIR, "microservices.json")
 SIMULATION_FILE = os.path.join(BASE_DIR, "simulation.json")
 
-# Vérification et chargement des fichiers JSON
+# Checking and loading JSON files
 def load_json(file_path):
-    """ Charge un fichier JSON et vérifie son existence """
+    """ Loads a JSON file and checks for its existence """
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Le fichier {file_path} est introuvable.")
 
     with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
-# Chargement des données
+# Loading data
 microservices = load_json(MICROSERVICES_FILE)
-simulation = load_json(SIMULATION_FILE)
+response = load_json(SIMULATION_FILE)
 
-def send_teams_message(service, item, status):
-    """ Envoie une alerte sur Teams en fonction de l'état du microservice """
+def send_teams_message(service, response_code, response_text, status):
+    """ Sends an alert to Teams based on the microservice status """
     try:
         response = pymsteams.connectorcard(WEBHOOK_URL)
-        response.title("<b>⚠️ Notification d'alerte ⚠️</b>")
+        response.title("<b>⚠️ Alert notification ⚠️</b>")
 
         if is_alert_active(service, status):
             return
 
         response.text(
-            f"<u>Micro service</u> : {service} <br>"
-            f"{format_message(status, item)} <br>"
-            f"<u>Code</u> : <span style='color:{get_status_color(status)}'>{item['status_code']}</span>"
+            f"<u>MicroService</u> : {service} <br>"
+            f"{format_message(status, response_text)} <br>"
+            f"<u>Code</u> : <span style='color:{get_status_color(status)}'>{response_code}</span>"
         )
         change_status(service, status)
 
         assert response.send()
-        print(f"Message envoyé avec succès sur Teams pour {service}.")
+        print(f"Message successfully sent on Teams for {service}.")
 
     except Exception as e:
-        print(f"Une erreur est survenue lors de l'envoi du message Teams : {e}")
+        print(f"An error occurred while sending the Teams message : {e}")
 
 def get_status_color(status):
-    """ Retourne la couleur associée au statut """
+    """ Returns the color associated with the status """
     return "green" if status == "Healthy" else "red"
 
 def change_status(service, status):
-    """ Met à jour le statut du microservice et sauvegarde le JSON """
+    """ Updates the microservice status and saves the JSON """
     microservices[service]["status"] = status
 
     with open(MICROSERVICES_FILE, "w", encoding="utf-8") as json_file:
         json.dump(microservices, json_file, indent=4)
 
-    print(f"Statut mis à jour : {service} -> {status}")
+    print(f"Status updated : {service} -> {status}")
 
-def format_message(status, item):
-    """ Génère le message à afficher selon l'état du microservice """
+def format_message(status, response_text):
+    """ Generates the message to be displayed depending on the state of the microservice """
     if status == "Healthy":
-        return "<u>Message</u> : Le microservice est de nouveau fonctionnel."
-    return f"<u>Message d'erreur</u> : {item['message_error']}"
+        return "<u>Message</u> : Service is WORKING fine !"
+    return f"<u>Error message</u> : Service is BROKEN ! ({response_text})"
 
-def check_condition():
-    """ Vérifie l'état des microservices et déclenche une alerte si nécessaire """
-    for index, service in enumerate(microservices):
+def check_health():
+    """ Checks the status of microservices and triggers an alert if necessary """
+
+    environment_broken = False
+    index=0
+
+    for service in microservices:
+        #health_url = BASE_ADDRESS + HEALTH_ENDPOINT_TEMPLATE.format(service)
         current_status = microservices[service]["status"]
-        new_status = "Healthy" if simulation[index]['status_code'] == 200 else "Unhealthy"
+        index += 1
+        try:
+            #response = requests.get(health_url, verify=False)
+            if response[index]['status_code'] == 200 and response[index]['text'] == "-":
+                status = "Healthy"
+            else:
+                status = "Unhealthy"
+                environment_broken = True
+            response_code = response[index]['status_code']
+            response_text = response[index]['text']
+            if current_status != status:
+                print(f"{service}: Change detected → {status}")
+                send_teams_message(service, response_code, response_text, status)
+        except requests.exceptions.RequestException as e:
+            status = f"Error ({str(e)})"
+            environment_broken = True
+        print(f"{service}: {status}")
 
-        if current_status == new_status:
-            print(f"{service}: Aucun changement")
-            continue
-
-        print(f"{service}: Changement détecté → {new_status}")
-        send_teams_message(service, simulation[index], new_status)
+    if environment_broken:
+        print("\033[91mEnvironment is BROKEN!\033[0m")  # Red message
+    else:
+        print("\033[92mEnvironment is WORKING fine!\033[0m")  # Green message
 
 def is_alert_active(service, status):
-    """ Vérifie si une alerte est déjà active pour éviter les doublons """
+    """ Checks if an alert is already active to avoid duplicates """
     return microservices[service]["status"] == status
 
 if __name__ == '__main__':
-    check_condition()
+    check_health()
